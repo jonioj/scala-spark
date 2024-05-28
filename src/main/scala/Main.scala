467bb05e0dd3a58e03b1c7fdbeb3a2b88a394893
jonaszlaba <jonaszlaba@cotahealthcare.com>
2024-05-28 10:43:42 +0200

    Creating temp view and using sql expression

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,7 @@
 package pl.jonasz
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types.StringType
+import org.apache.spark.sql.{DataFrame, SparkSession}
 
 object Main {
   def main(args: Array[String]): Unit = {
@@ -9,12 +10,18 @@ object Main {
       .master("local[*]")
       .config("spark.driver.bindAddress","127.0.0.1")
       .getOrCreate()
-  val df = spark.read
+  val df: DataFrame = spark.read
     .option("header", value = true)
     .option("inferSchema", value = true)
     .csv("data/AAPL.csv")
 
-    df.show()
+    //df.show()
     df.printSchema()
+    // df.select("Date","Open","Close").show()
+    val column = df("Open")
+    val newColumn = column + (2.0)
+    val columnString = column.cast(StringType)
+    df.createTempView("df")
+    spark.sql("select mean(High) from df").show()
   }
 }
